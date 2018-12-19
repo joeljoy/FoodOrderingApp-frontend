57bd95dcb658db6bb4b269d598b7aa38c64d8554
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2018-12-19 19:13:03 +0530

    completed menu item section

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -11,6 +11,9 @@ import ShoppingCart from '@material-ui/icons/ShoppingCart';
 import Add from '@material-ui/icons/Add';
 import Badge from '@material-ui/core/Badge';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
 
 const styles = {
   card: {
@@ -41,6 +44,8 @@ class Details extends React.Component {
       numberUsersRated: null,
       locality: null,
       categories: [],
+      itemAddedBarOpen: false,
+      cartItems: 0,
     }
   };
 
@@ -72,6 +77,19 @@ class Details extends React.Component {
       console.log('error getting data', error);
     });
   };
+
+  handleItemAddedBar = () => {
+    this.setState({
+      itemAddedBarOpen: !this.state.itemAddedBarOpen
+    })
+  }
+
+  addItemOnClickHandler = (item) => {
+    this.handleItemAddedBar();
+    this.setState({
+      cartItems: this.state.cartItems + 1,
+    })
+  }
 
   render(){
     return(
@@ -121,7 +139,7 @@ class Details extends React.Component {
           <div style={{padding:'3%'}}>
             {this.state.categories.map(categoryItem =>
               <div key={categoryItem.id}>
-                <CategoryItem item={categoryItem} />
+                <CategoryItem item={categoryItem} this={this} />
               </div>
             )}
           </div>
@@ -133,7 +151,7 @@ class Details extends React.Component {
             <Card className={styles.card}>
               <CardContent>
                 <div style={{display:"inline-block", width:"100%"}}>
-                  <div style={{float:"left", width:"10%"}}><Badge badgeContent={0} color="primary"><ShoppingCart /></Badge></div>
+                  <div style={{float:"left", width:"10%"}}><Badge badgeContent={this.state.cartItems} color="primary"><ShoppingCart /></Badge></div>
                   <div style={{float:"right", width:"90%"}}><Typography variant="h5" gutterBottom style={{fontWeight:'bold'}}> My Cart </Typography></div>
                 </div>
                 <div style={{display:"inline-block", width:"100%"}}>
@@ -151,35 +169,61 @@ class Details extends React.Component {
         </div>
       </div>
 
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={this.state.itemAddedBarOpen}
+        autoHideDuration={6000}
+        onClose={this.handleItemAddedBar}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">Item added to cart!</span>}
+        action={[
+          <IconButton
+            key="close"
+            aria-label="Close"
+            color="inherit"
+            onClick={this.handleItemAddedBar}
+          >
+          <CloseIcon />
+          </IconButton>,
+        ]}
+      />
+
       </div>
     );
-
-    function CategoryItem(props) {
-      return (
-        <div style={{padding:"1%"}}>
-          <Typography variant="caption" gutterBottom style={{fontWeight:"bold", textTransform:"uppercase"}}> {props.item.categoryName} </Typography>
-          <Divider />
-          {props.item.items.map(menuItem =>
-            <div key={menuItem.id}>
-              <MenuItem item={menuItem} />
-            </div>
-          )}
-        </div>
-      )
-    };
-
-    function MenuItem(props) {
-      const color = props.item.type === "Non-Veg" ? "red" : "green"
-      return (
-        <div style={{display:"flex", flexDirection:"row", width:"100%", padding:"2%"}}>
-          <div style={{width:"5%", display:"flex", alignItems:"center"}}><i style={{color:color}} class="fa fa-circle" aria-hidden="true"></i></div>
-          <div style={{width:"65%", display:"flex", alignItems:"center", textTransform:"capitalize"}}> {props.item.itemName} </div>
-          <div style={{width:"20%", display:"flex", alignItems:"center"}}><i class="fa fa-inr" aria-hidden="true"> {props.item.price.toFixed(2)} </i></div>
-          <div style={{width:"10%", display:"flex", alignItems:"center"}}><Add style={{height:"100%"}} /></div>
-        </div>
-      )
-    }
   }
 }
+
+function CategoryItem(props) {
+  return (
+    <div style={{padding:"1%"}}>
+      <Typography variant="caption" gutterBottom style={{fontWeight:"bold", textTransform:"uppercase"}}> {props.item.categoryName} </Typography>
+      <Divider />
+      {props.item.items.map(menuItem =>
+        <div key={menuItem.id}>
+          <MenuItem item={menuItem} this={props.this} />
+        </div>
+      )}
+    </div>
+  )
+};
+
+function MenuItem(props) {
+  const color = props.item.type === "Non-Veg" ? "red" : "green"
+  return (
+    <div style={{display:"flex", flexDirection:"row", width:"100%", padding:"2%"}}>
+      <div style={{width:"5%", display:"flex", alignItems:"center"}}><i style={{color:color}} class="fa fa-circle" aria-hidden="true"></i></div>
+      <div style={{width:"65%", display:"flex", alignItems:"center", textTransform:"capitalize"}}> {props.item.itemName} </div>
+      <div style={{width:"20%", display:"flex", alignItems:"center"}}><i class="fa fa-inr" aria-hidden="true"> {props.item.price.toFixed(2)} </i></div>
+      <div style={{width:"10%", display:"flex", alignItems:"center"}}>
+        <IconButton onClick={props.this.addItemOnClickHandler}><Add style={{height:"100%"}} /></IconButton>
+      </div>
+    </div>
+  )
+};
 
 export default Details;
